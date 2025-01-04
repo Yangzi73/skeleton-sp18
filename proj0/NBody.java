ae149c8bde9760bb0168832f9982375a6c183c2f
Tsao <1142190666@qq.com>
2025-01-04 15:24:43 +0800

    update a method of ReadPlanets

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -7,6 +7,23 @@ public class NBody {
         return Radius;
     }
 
+    public static Planet[] readPlanets(String fileAddress){
+        In in = new In(fileAddress);
+        int Planet_nums = in.readInt();
+        double Radius = in.readDouble();
+        Planet[] planets = new Planet[Planet_nums];
+        for (int i = 0 ; i < Planet_nums; i++){
+            double xxPos = in.readDouble();
+            double yyPos = in.readDouble();
+            double xxVel = in.readDouble();
+            double yyVel = in.readDouble();
+            double mass = in.readDouble();
+            String imgFileName = in.readString();
+            planets[i] = new Planet(xxPos, yyPos, xxVel, yyVel, mass, imgFileName);
+        }
+        return planets;
+    }
+
     public static void main(String[] args) {
         readRadius("data/planets.txt");
     }
